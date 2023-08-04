90da0e4f2d1cc9fb3fc677286cde76ef2b7aa3cd
Jennifer <jenniferbesson13@hotmail.com>
2023-08-04 15:52:19 +0200

    add link to menu

--- src/containers/Menu/index.js
+++ src/containers/Menu/index.js
@@ -7,16 +7,10 @@ import "./style.scss";
 const Menu = () => (
   <nav>
     <Logo />
-    <ul>
-      <li>
-        <a href="#nos-services">Nos services</a>
-      </li>
-      <li>
-        <a href="#nos-realisations">Nos réalisations</a>
-      </li>
-      <li>
-        <a href="#notre-equipe">Notre équipe</a>
-      </li>
+    <ul>    
+    <li><a href="/#nos-services">Nos services</a></li>
+    <li><a href="/#nos-realisations">Nos réalisations</a></li>
+    <li><a href="/#notre-equipe">Notre équipe</a></li>
     </ul>
     <Button title="contact" onClick={() => (window.document.location.hash = "#contact")}>
       Contact
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,7 @@ import ServiceCard from "../../components/ServiceCard";
 import EventCard from "../../components/EventCard";
 import PeopleCard from "../../components/PeopleCard";
 
+
 import "./style.scss";
 import EventList from "../../containers/Events";
 import Slider from "../../containers/Slider";
@@ -12,17 +13,18 @@ import Form from "../../containers/Form";
 import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
+
 const Page = () => {
   const {last} = useData()
   return <>
     <header>
-      <Menu />
+    <Menu />
     </header>
     <main>
       <section className="SliderContainer">
         <Slider />
       </section>
-      <section className="ServicesContainer">
+      <section className="ServicesContainer" id="nos-services">
         <h2 className="Title">Nos services</h2>
         <p>Nous organisons des événements sur mesure partout dans le monde</p>
         <div className="ListContainer">
@@ -51,11 +53,11 @@ const Page = () => {
           </ServiceCard>
         </div>
       </section>
-      <section className="EventsContainer">
+      <section className="EventsContainer" id="nos-realisations">
         <h2 className="Title">Nos réalisations</h2>
         <EventList />
       </section>
-      <section className="PeoplesContainer">
+      <section className="PeoplesContainer" id="notre-equipe">
         <h2 className="Title">Notre équipe</h2>
         <p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
         <div className="ListContainer">
